6fd0cd83e2dcac25d5effab9bfdca169aa579842
navee <navee@LAPTOP-OHQ3ME1T>
2022-06-09 18:45:55 +0530

    java updated

---
+++ Hello/src/Demo/Test.java
@@ -0,0 +1,27 @@
+package Demo;
+
+public class Test {
+       
+ public static void main (String[]args)
+  {
+
+	 int a=10;
+	 int b=20;
+	 int x=a+b;
+	 int y=a-b;
+	 int z=a*b;
+	 int d=a/b;
+	 int e=a%b; 
+	 
+	 System.out.println("add"+x);
+	 System.out.println("sub"+y);
+	 System.out.println("mult"+z);
+	 System.out.println("divi"+d);
+     System.out.println("modulor"+e);					 
+    			  
+      }
+}
+   
+
+   
+   
---
+++ Hello/src/Example/Test.java
@@ -0,0 +1,59 @@
+package Example;
+
+public class Test {
+  /*
+	int b=40;
+	static int empid =70;
+     public void m1()
+     {
+		int a=60;
+	System.out.println("m1 vamsi");
+	System.out.println("a local varible");
+	System.out.println("b instance varible");
+	System.out.println("empid static value");
+	
+     }
+     public void m2()
+     {
+     System.out.println("m2 vamsi");
+   	 System.out.println("b instance varible");
+ 	 System.out.println("empid static value");
+     }
+
+      public void m3()
+     {
+     System.out.println("m3 vamsi");
+ 	 System.out.println("empid static value");
+ 	 
+     }
+     public static void main(String[]args)
+     {
+     System.out.println("gopi");
+     Test obj=new Test ();
+    		 obj.m1();
+    		 obj.m2();
+    		 obj.m3();
+}
+}
+    */		 
+    		 
+    		 
+    		 
+
+	   int x=10, y=15;
+	   if (11<10 || 16>15) { 
+		   x++;
+	   }
+	   else {
+		   y++;
+	   }
+	System.out.println(x+"____"+y);
+	} 
+    		 
+    		 
+    		 
+    		 
+    		 
+    		 
+    		 
+     
